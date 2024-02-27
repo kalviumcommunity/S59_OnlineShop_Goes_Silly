f7a2efc463668eaa903e874352b4e41a349a6dc5
ShubhamThakur025 <thakur0805@gmail.com>
2024-02-27 17:16:16 +0530

    connected to db

---
+++ db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const connectToMongoDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.USER_KEY}@cluster0.xhdkcyb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, {
+        });
+        console.log('Connected to mongoDB');
+    } catch (err) {
+        console.error('Error connecting to mongoDB:', err.message);
+    }
+};
+
+const checkConnection = () => {
+    return mongoose.connection.readyState === 1;
+}
+
+module.exports = {
+    connectToMongoDB,
+    checkConnection,
+};
--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express')
+const mongoose = require("mongoose");
+const { connectToMongoDB, checkConnection } = require('./db');
 const app = express()
 const port = 8080
 
-app.listen(port, ()=>{
-    console.log("Running on PORT", port);
-});
 
-app.get('/ping', (req, res) =>{
-    try{
+app.get('/ping', (req, res) => {
+    try {
         res.send("Pong");
     }
-    catch(error){
+    catch (error) {
         console.log("Error:", error)
     }
+});
+
+app.get("/", (req, res) => {
+    if (checkConnection()) {
+        res.send("CONNECTION STATUS : Connected To MongoDB")
+    } else {
+        res.send("CONNECTION STATUS : Failed To Connect With MongoDB")
+    }
+});
+
+connectToMongoDB()
+
+app.listen(port, () => {
+    console.log("Running on PORT", port);
 });
