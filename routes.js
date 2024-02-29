0847e2bcd1fa74148b9d068da19fa78db27a8280
ShubhamThakur025 <thakur0805@gmail.com>
2024-02-29 14:55:30 +0530

    Schema and various endpoints added

---
+++ routes.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const router = express.Router();
+const { connectToMongoDB } = require('./db.js')
+const product = require('./schema.js')
+
+router.get('/', async (req, res) => {
+    try {
+        const products = await product.find()
+        res.json(products)
+    }
+    catch (err) {
+        res.json({ error: "An Error occurred" })
+    }
+})
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const foundProduct = await product.findById(req.params.id)
+        res.json(foundProduct)
+
+    }
+    catch (err) {
+        res.json({ error: "An Error occurred" })
+
+    }
+
+});
+
+router.post('/add-items', async (req, res) => {
+    const newProduct = new product({
+        productID: req.body.productID,
+        productName: req.body.productName,
+        category: req.body.category,
+        catId: req.body.catID,
+    })
+    try {
+        const savedProd = await newProduct.save()
+        res.json(savedProd)
+    }
+    catch (err) {
+        res.json({ error: "An Error occurred" })
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const foundProduct = await product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!foundProduct) {
+            return res.status(404).json({ error: "Product Not Found" })
+        }
+        res.json(foundProduct)
+
+    }
+    catch (err) {
+        res.status(500).send('Error: ' + err);
+
+    }
+
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const foundProduct = await product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!hat) {
+            return res.status(404).json({ error: "Product Not Found" })
+        }
+        res.json(foundProduct);
+    } catch (err) {
+        res.status(500).send('Error: ' + err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const hat = await product.findByIdAndDelete(req.params.id);
+        if (!hat) {
+            return res.status(404).json({ error: "Product Not Found" })
+        }
+        res.send('Product deleted');
+    } catch (err) {
+        res.status(500).send('Error: ' + err);
+    }
+});
+
+connectToMongoDB()
+
+module.exports = router
---
+++ schema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const productsSchema = new mongoose.Schema({
+    productID : {
+        type: Number,
+        required : true
+    },
+    productName : {
+        type: String,
+        required : true
+    },
+    category : {
+        type: String,
+        required: true
+    },
+    catId : {
+        type: Number,
+        required: true
+    }
+})
+
+const product = mongoose.model('product', productsSchema)
+module.exports = product
