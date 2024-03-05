3a0ad6b0ddbdd8a6bc5b3b871426d7a0ae15b4d6
ShubhamThakur025 <thakur0805@gmail.com>
2024-03-05 10:29:09 +0530

    Source for images added in schema

--- backend/schema.js
+++ backend/schema.js
@@ -9,6 +9,10 @@ const productsSchema = new mongoose.Schema({
         type: String,
         required : true
     },
+    prodSrc : {
+        type: String,
+        required : true
+    },
     category : {
         type: String,
         required: true
