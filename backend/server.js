45e2fad388d9688038c753896ffcf7505b57b1b7
ShubhamThakur025 <thakur0805@gmail.com>
2024-03-05 10:53:40 +0530

    cors middleware added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mongoose = require("mongoose");
+const cors = require("cors")
 const { connectToMongoDB, checkConnection } = require('./db');
 const routes = require('./routes.js')
 const app = express()
 const port = 8080
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', routes)
 
