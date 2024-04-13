d9aab5123408d16cdb05a3e9e41c9e3dced2a88d
ShubhamThakur025 <thakur0805@gmail.com>
2024-04-13 11:35:51 +0530

    Unnecessary console logs removed to ensure security

--- backend/routes.js
+++ backend/routes.js
@@ -63,7 +63,6 @@ router.post('/register', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-    console.log(req.body)
     const findUser = await user.findOne({ mail: req.body.mail})
     if (findUser) {
         const isMatch = await bcrypt.compare(req.body.password, findUser.password)
